8369ef63ee4e5232657b54633990fd1c47882b64
Pat Wangrungarun <phatograph@gmail.com>
2018-10-31 10:33:05 +0700

    Update policy pages

--- custom-page.php
+++ custom-page.php
@@ -10,21 +10,6 @@
 <div class="Main">
   <div class="container">
     <div class="Contact">
-      <dl class="Contact__left">
-        <div class="Contact__left__item">
-          <dt>Email:</dt>
-          <dd>info@series.tech</dd>
-        </div>
-        <div class="Contact__left__item">
-          <dt>Tel:</dt>
-          <dd>02-024-9119</dd>
-        </div>
-        <div class="Contact__left__item Contact__left__item--full">
-          <dt>Address:</dt>
-          <dd>725 Metropolis Building ชั้น10 ห้อง 1001 ถนนสุขุมวิท แขวงคลองตันเหนือ เขตวัฒนา กรุงเทพฯ 10110</dd>
-        </div>
-      </dl>
-
       <div class="Contact__right">
         <?php while (have_posts()) : the_post(); ?>
           <?php the_content() ?>
