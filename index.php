c1e0ac1c0dc13729d955e005c17a55e3c204cc68
Pat Wangrungarun <phatograph@gmail.com>
2018-10-31 10:53:35 +0700

    FB/TW links

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
       <div class="Header__top">
         <ul class="Header__top__ul">
           <li>Follow us at</li>
-          <li><a class="Header__top__ul__a"></a></li>
-          <li><a class="Header__top__ul__a Header__top__ul__a--twitter"></a></li>
+          <li><a class="Header__top__ul__a" href="https://www.facebook.com/series.tech" target="_blank" rel="nofollow"></a></li>
+          <li><a class="Header__top__ul__a Header__top__ul__a--twitter" href="https://twitter.com/seriesdottech" target="_blank" rel="nofollow"></a></li>
         </ul>
 
         <form class="Header__top__search" action="<?php echo site_url(); ?>">
@@ -59,8 +59,8 @@
           <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
 
           <div class="Footer__footer__nav__right">
-            <a class="Footer__footer__nav__a"></a>
-            <a class="Footer__footer__nav__a Footer__footer__nav__a--twitter"></a>
+            <a class="Footer__footer__nav__a" href="https://www.facebook.com/series.tech" target="_blank" rel="nofollow"></a>
+            <a class="Footer__footer__nav__a Footer__footer__nav__a--twitter" href="https://twitter.com/seriesdottech" target="_blank" rel="nofollow"></a>
           </div>
         </div>
 
