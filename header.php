14dfd6045b074e8af877eded3b27b4d52f19a9b1
Pat Wangrungarun <phatograph@gmail.com>
2018-12-12 12:23:13 +0700

    V 1.6.5

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <title>Series <?php wp_title('|'); ?></title>
 
-    <meta name="version" content="1.6.4" />
+    <meta name="version" content="1.6.5" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
     <meta property="og:image" content="<?php the_post_thumbnail_url(); ?>" />
 
